75819d390a0c6b47eafccf4565d3a008bff4931d
Cristhian Perez Barrientos <cperezb@comteco.com.bo>
2026-06-09 09:30:18 -0400

    anular la creacion de super usuario

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,21 +19,21 @@ echo "Recolectando archivos estáticos..."
 python manage.py collectstatic --noinput --clear --no-post-process
 
 # 4. Crear superusuario de forma segura (Automático)
-echo "Creando/actualizando superusuario..."
-python manage.py shell << EOF
-import os
-from django.contrib.auth import get_user_model
-User = get_user_model()
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'cperezb')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'cperezb@comteco.com.bo')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+# echo "Creando/actualizando superusuario..."
+# python manage.py shell << EOF
+# import os
+# from django.contrib.auth import get_user_model
+# User = get_user_model()
+# username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'cperezb')
+# email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'cperezb@comteco.com.bo')
+# password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print(f"Superusuario '{username}' creado exitosamente!")
-else:
-    print(f"Superusuario '{username}' ya existe.")
-EOF
+# if not User.objects.filter(username=username).exists():
+#     User.objects.create_superuser(username, email, password)
+#     print(f"Superusuario '{username}' creado exitosamente!")
+# else:
+#     print(f"Superusuario '{username}' ya existe.")
+# EOF
 
 echo "Inicialización completada exitosamente!"
 
